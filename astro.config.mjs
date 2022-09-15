4451a326553baab7f6db17ffa53ba0800d48b8e0
Artur Janas <artur.abato.janas@gmail.com>
2022-09-15 11:45:38 +0100

    feat: adds GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [
+		tailwind(),
+		partytown({
+			// Adds dataLayer.push as a forwarding-event.
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
 	output: "server",
 	adapter: netlify(),
 });
